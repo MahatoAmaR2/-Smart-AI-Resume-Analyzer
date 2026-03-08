e58e1458ad3f1aeb592e5115404bd7bbcf6eb34d
MahatoAmaR2 <bauaa476@gmail.com>
2026-03-08 16:18:46 +0530

    Set up database connection and environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
-dotenv.config();
+import dns from "node:dns";
 import app from "./src/app.js";
+import connectDB from "./src/config/connectDB.js";
+
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+dotenv.config({ path: "./.env" });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
-
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:", err.message);
+  });
