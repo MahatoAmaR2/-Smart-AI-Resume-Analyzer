d69c3a960119013b447c1cd70eede01d76546906
MahatoAmaR2 <bauaa476@gmail.com>
2026-03-10 00:45:45 +0530

    update auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import TokenBlacklist from "../models/blacklist.model.js";
 
 async function authUser(req, res, next) {
   try {
@@ -6,7 +7,12 @@ async function authUser(req, res, next) {
     if (!token) {
       return res.status(401).json({ message: "No token provided" });
     }
-
+    // Check if the token is blacklisted
+    const isBlacklistedToken = await TokenBlacklist.findOne({ token });
+    if (isBlacklistedToken) {
+      return res.status(401).json({ message: "Token is invalid" });
+    }
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
