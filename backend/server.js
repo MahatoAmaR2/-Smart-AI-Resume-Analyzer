10b60470b75483dc333a389ad3fd3f5e08a5f8da
MahatoAmaR2 <bauaa476@gmail.com>
2026-03-20 17:30:48 +0530

    Added gemin api

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,11 @@ import dns from "node:dns";
 import app from "./src/app.js";
 import connectDB from "./src/config/connectDB.js";
 
-
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 dotenv.config({ path: "./.env" });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
